0986aecc11df923be4c35d260faceaf7648f87ff
lily-elephant <xrzslixiangqun@163.com>
2019-05-18 21:43:46 +0800

    固定勤务

--- page/fixed-duty/fixed-duty.js
+++ page/fixed-duty/fixed-duty.js
@@ -1,5 +1,5 @@
-function play(rtspUrl, id) {
-    var vlc = document.getElementById(id);
+function play(rtspUrl, domId) {
+    var vlc = document.getElementById(domId);
     var options = new Array(":aspect-ratio=16:9", "--rtsp-tcp");
     var id = vlc.playlist.add(rtspUrl, "fancy name", options);
     vlc.playlist.playItem(id);
