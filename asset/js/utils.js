a07f2a09c5607c91a76c8bd0f37a75ec62ede5d2
up2day <vivadj@qq.com>
2019-05-18 22:02:35 +0800

    执勤排班

--- asset/js/utils.js
+++ asset/js/utils.js
@@ -36,3 +36,30 @@ var _top = 100
 //     })
 // }, 500)
 })
+
+//格式化时间戳
+function formatDate(t, isTime) {
+  var date;
+  if(Number(t)){
+    //formatDate(1398250569555)
+    //formatDate('1398250569555')
+    date = new Date(Number(t));
+  }else{
+    //参数  t 如果不传或格式错误，则格式化当前系统时间
+    //formatDate()
+    //formatDate(null)
+    //formatDate(NaN, 1)
+    //formatDate('d1398250569555')
+    date = new Date();
+  }
+  var YY = date.getFullYear();
+  var MM = (date.getMonth()+1 < 10 ? '0'+ (date.getMonth()+1) : date.getMonth()+1);
+  var DD = date.getDate()+1 < 10 ? '0'+ date.getDate() : date.getDate();
+  var hh = date.getHours() < 10 ? '0'+ date.getHours() : date.getHours();
+  var mm = date.getMinutes() < 10 ? '0'+ date.getMinutes() : date.getMinutes();
+  var ss = date.getSeconds() < 10 ? '0'+ date.getSeconds() : date.getSeconds();
+  //isTime = true 日期格式为 YY-MM-DD hh:mm:ss;
+  //isTime = false 日期格式为 YY-MM-DD;
+  var ft = isTime ? (YY + '-' + MM + '-' + DD + ' ' + hh + ':' + mm + ':' + ss) : (YY + '-' + MM + '-' + DD);
+  return ft;
+}
