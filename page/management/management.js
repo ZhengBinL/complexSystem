51e09898e3af171d28d6991c5b5569cf3a22c21b
ZhengBinL <774037817@qq.com>
2019-05-20 14:49:54 +0800

    侧边栏与树的修改1

--- page/management/management.js
+++ page/management/management.js
@@ -1,39 +1,8 @@
 
 $(function () {
-    // 侧边栏展开收起
-    $('.icon-fold').click(function () {
-        // console.log($(this).find('#showFlag')[0].alt,'src')
-        var html = $(this).find('#showFlag').attr('alt')
-        if (html == '1') {
-            $(this).find('#showFlag').attr('alt', '2')
-            $(this).find('#showFlag').attr('src', '../../asset/img/icon_expand.png')
-            $(this).animate({
-                left: '264px'
-            })
-            $('.aside').animate({
-                left: '0'
-            })
-            $('#zhanwei-c').animate({
-                left: '0'
-            })
-        } else {
-            $('.aside').animate({
-                left: '-264px'
-            })
-            $(this).find('#showFlag').attr('alt', '1')
-            $(this).find('#showFlag').attr('src', '../../asset/img/icon_fold.png')
-            $(this).animate({
-                left: '18px'
-            })
-            $('#zhanwei-c').animate({
-                left: '-300px'
-            })
-        }
-    })
-
-
     // 菜单树
-    var zTreeObj;
+    var zNodes = []//节点
+    var indexNum//点击的某个点的index
     // zTree 的参数配置，深入使用请参考 API 文档（setting 配置详解）
     var setting = {
         view: {
@@ -52,44 +21,26 @@ $(function () {
             }
         },
         callback: {
-            onClick: zTreeOnClick
+            onClick: zTreeOnClick//节点点击回调函数
         }
     };
-    var zNodes = []
-    var url = "http://192.168.3.8:18080"
-    $.get("http://172.16.5.226:18080/resource/node", function (res) {
+    
+    $.get($ctx+"/resource/node", function (res) {
         zNodes = res.data
         $.fn.zTree.init($("#menu-tree"), setting, zNodes);
     })
-    var indexNum
 
     function zTreeOnClick(event, treeId, treeNode) {
-        // console.log(treeNode,'treeNode')
-        $.get('http://172.16.5.226:18080/dvr/rtspUrl?id=1000000', function (res) {
+        if(!!treeNode.isParent)return
+        $.get($ctx+'/dvr/rtspUrl?id=1000000', function (res) {
             if (indexNum == 1) {
-                var vlc = document.getElementById("vlc1");
-                var options = new Array(":aspect-ratio=16:9", "--rtsp-tcp");
-                var id = vlc.playlist.add(res.data.rtspUrl, "fancy name", options);
-                vlc.playlist.playItem(id);
-                vlc.playlist.play();
+                play($('#vlc1'))
             } else if (indexNum == 2) {
-                var vlc = document.getElementById("vlc2");
-                var options = new Array(":aspect-ratio=16:9", "--rtsp-tcp");
-                var id = vlc.playlist.add(res.data.rtspUrl, "fancy name", options);
-                vlc.playlist.playItem(id);
-                vlc.playlist.play();
+                play($('#vlc2'))
             } else if (indexNum == 3) {
-                var vlc = document.getElementById("vlc3");
-                var options = new Array(":aspect-ratio=16:9", "--rtsp-tcp");
-                var id = vlc.playlist.add(res.data.rtspUrl, "fancy name", options);
-                vlc.playlist.playItem(id);
-                vlc.playlist.play();
+                play($('#vlc3'))
             } else if (indexNum == 4) {
-                var vlc = document.getElementById("vlc4");
-                var options = new Array(":aspect-ratio=16:9", "--rtsp-tcp");
-                var id = vlc.playlist.add(res.data.rtspUrl, "fancy name", options);
-                vlc.playlist.playItem(id);
-                vlc.playlist.play();
+                play($('#vlc4'))
             }
             $('.videos-li').eq(indexNum-1).find('.icon-history').attr('data-szCameraId',res.data.cid)
         })
@@ -137,32 +88,14 @@ $(function () {
                 "width": "100%",
                 "height": "100%"
             })
-        }
-        
+        } 
     })
 
-    function play(rtspUrl) {
-        var vlc
-        if (indexNum == 1) {
-            vlc = document.getElementById("vlc1");
-        } else if (indexNum == 2) {
-            vlc = document.getElementById("vlc2");
-        } else if (indexNum == 3) {
-            vlc = document.getElementById("vlc3");
-        } else if (indexNum == 4) {
-            vlc = document.getElementById("vlc4");
-        }
-        // var vlc = document.getElementById("vlc1");
+    function play(vlc){
         var options = new Array(":aspect-ratio=16:9", "--rtsp-tcp");
-        var id = vlc.playlist.add(rtspUrl, "fancy name", options);
+        var id = vlc.playlist.add(res.data.rtspUrl, "fancy name", options);
         vlc.playlist.playItem(id);
         vlc.playlist.play();
     }
-
-    function stop() {
-        var vlc = document.getElementById("vlc1");
-        vlc.playlist.stop();
-    }
-    
 })
     
