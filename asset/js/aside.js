5f31f6f5c77ba81b935e107fdefb86f82e6febd8
up2day <vivadj@qq.com>
2019-05-21 13:58:43 +0800

    树样式+jq库

--- asset/js/aside.js
+++ asset/js/aside.js
@@ -1,51 +1,42 @@
 $(function () {
       // 侧边栏展开收起
-      $('.icon-fold').click(function () {
-            // console.log($(this).find('#showFlag')[0].alt,'src')
-            var html = $(this).find('#showFlag').attr('alt')
-            if (html == '1') {
-                  $(this).find('#showFlag').attr('alt', '2')
-                  $(this).find('#showFlag').attr('src', '../../asset/img/icon_expand.png')
-                  $(this).animate({
-                        left: '264px'
-                  })
-                  $('.aside').animate({
-                        left: '0'
-                  })
-            } else {
-                  $('.aside').animate({
-                        left: '-264px'
-                  })
-                  $(this).find('#showFlag').attr('alt', '1')
-                  $(this).find('#showFlag').attr('src', '../../asset/img/icon_fold.png')
-                  $(this).animate({
-                        left: '18px'
-                  })
-            }
+  $('#showFlag').click(function () {
+    $tis = $(this);
+    if($tis.hasClass('unfolded')) {
+      $('.aside').animate({
+        left: '0'
       })
+      $tis.removeClass('unfolded');
+    } else {
+      $('.aside').animate({
+        left: '-450px'
+      })
+      $tis.addClass('unfolded');
+    }
+  })
 
 
       // 菜单树
-      // var setting = {
-      //       data:{
-      //         view:{
-      //           fontCss:{
-      //             "color":"#229fe6"
-      //           }
-      //         },
-      //         simpleData:{
-      //           enable:true,
-      //           idKey: "id",
-      //           pIdKey: "pid",
-      //           rootPId: 0
-      //         }
-      //       }
-      // };
-      // var zNodes =[];
-      // $.get($ctx + "/resource/node",function(res){
-      //       zNodes =res.data
-      //       console.log(zNodes,'sdadwe')
-      //       $.fn.zTree.init($("#menu-tree"), setting,zNodes);
-      // })
+      var setting = {
+            data:{
+              view:{
+                fontCss:{
+                  "color": "#229fe6"
+                },
+                showLine:true
+              },
+              simpleData: {
+                enable: true,
+                idKey: "id",
+                pIdKey: "pid",
+                rootPId: 0
+              }
+            }
+      };
+      var zNodes =[];
+      $.get($ctx + "/resource/node",function(res){
+            zNodes =res.data
+            $.fn.zTree.init($("#menu-tree"), setting,zNodes);
+      })
       
 })
