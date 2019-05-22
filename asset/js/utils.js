bc7062cb6f8a9f6b51dc5e5e7fda0444279d9b33
lily-elephant <xrzslixiangqun@163.com>
2019-05-22 22:31:39 +0800

    报警整理

--- asset/js/utils.js
+++ asset/js/utils.js
@@ -7,6 +7,8 @@ $(function(){
     $(this).addClass('on').siblings().removeClass('on');
     $('.js-tabmain > li').eq(liindex).addClass('on').siblings().removeClass('on');
   });
+})
+
 /*--------------------
 ** 报警弹框
 * *-------------------*/
@@ -26,20 +28,16 @@ $('body').on('click', '.js-police-pop-close', function () {
     })
 }).on('click', '.js-police-look', function () {
     var msg = $(this).closest('.police-wrapper').find('.js-police-msg').html()
-    var rtspUrls = $(this).closest('.police-wrapper').find('.js-rtspUrls').html()
-    // setCookie('param', msg)
-  console.log(rtspUrls, 'rtspUrls')
-    console.log(document.cookie, 'cookieceshi')
-  return
+    var rtspUrls = $(this).closest('.police-wrapper').find('.js-rtspUrls').text()
+    var _encodeRtsp = encodeURIComponent(rtspUrls);
+    // console.log(_encodeRtsp, '_encodeRtsp');
     document.cookie="hrefParam="+ msg + ';path=/'; // 存储cookie
-    document.cookie="rtspUrls="+rtspUrls + ';path=/';
-     //console.log(document.cookie)
-    //var username=document.cookie.split(";")[0].split("=")[1];
-    window.location.href = '../../page/prevention/index.html?call=1'
+    document.cookie="rtspUrls="+_encodeRtsp + ';path=/';
+    //console.log(document.cookie);
+    window.location.href = '../../page/prevention/index.html?call=1';
 })
+
 var _top = 80
-var html_pop = ''
-		
 setInterval(function () {
     $.ajax({
         type:'GET',
@@ -49,20 +47,20 @@ setInterval(function () {
         success:function(res){
             if (res.code == 0 && res.data.msg){
                 _top += 10
+                var html_pop = ''
                 var data = JSON.parse(res.data.msg);
-                console.log(data)
                 html_pop += '<iframe id="zhanwei-police" src="about:blank" frameborder="0" marginheight="0" marginwidth="0" style="position: fixed;display: block;top:  '+ _top +'px;right: 30px;width: 290px;height: 302px;z-index: 0;background: transparent;"></iframe><div class="police-wrapper" style="top: '+ _top +'px;">' +
-                        '<div class="police-title">报警信息<img class="police-close js-police-pop-close" src="../../asset/img/icon-close.png" alt="关闭"></div>' +
-                        '<ul class="police-content">' +
-                        '<li><span>报警类型：</span><span class="police-detail">' + data.alarmType.systemOptionName +'</span></li>' +
-                        '<li><span>设备名称：</span><span class="police-detail">'+ data.alarmSource.iPDeviceName +'</span></li>' +
-                        '<li><span>报警时间：</span><span class="police-detail">'+ data.timeCreated +'</span></li></ul>' +
-                        '<div class="btn-wrapper">' +
-                        '<div style="display: none" class="js-police-msg">'+res.data.msg+'</div>' +
-                        '<div class="js-rtspUrls" style="display: none;">'+res.data.rtspUrls[0]+'</div>'+
-                        '<a href="javascript:;" class="js-police-look layui-btn layui-btn-sm layui-btn-normal" target="_self">查看</a>' +
-                        '<a href="javascript:;" class="js-police-pop-close layui-btn layui-btn-sm layui-btn-primary">关闭</a>' +
-                        '</div></div>'
+                    '<div class="police-title">报警信息<img class="police-close js-police-pop-close" src="../../asset/img/icon-close.png" alt="关闭"></div>' +
+                    '<ul class="police-content">' +
+                    '<li><span>报警类型：</span><span class="police-detail">' + data.alarmType.systemOptionName +'</span></li>' +
+                    '<li><span>设备名称：</span><span class="police-detail">'+ data.alarmSource.iPDeviceName +'</span></li>' +
+                    '<li><span>报警时间：</span><span class="police-detail">'+ data.timeCreated +'</span></li></ul>' +
+                    '<div class="btn-wrapper">' +
+                    '<div style="display: none" class="js-police-msg">'+res.data.msg+'</div>' +
+                    '<div class="js-rtspUrls" style="display: none;">'+res.data.rtspUrls+'</div>'+
+                    '<a href="javascript:;" class="js-police-look layui-btn layui-btn-sm layui-btn-normal" target="_self">查看</a>' +
+                    '<a href="javascript:;" class="js-police-pop-close layui-btn layui-btn-sm layui-btn-primary">关闭</a>' +
+                    '</div></div>'
                 $('body').append(html_pop)
             }
         },
@@ -71,29 +69,6 @@ setInterval(function () {
         }
     })
 }, 1000)
-
-})
-function setCookie(key,value,t)
-  { if(t>0){
-    var oDate=new Date();
-    oDate.setDate(oDate.getDate()+t);
-    document.cookie=key+"="+value+"; expires="+oDate.toDateString()+ ";path=/";
-  }
-}
-function removeCookie(key){
-  setCookie(key,"",-1);//把cookie设置为过期
-}
-
-
-
-function getCookie(name)
-{
-    var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
-    if(arr=document.cookie.match(reg))
-        return unescape(arr[2]);
-    else
-        return null;
-}
 
 //格式化时间戳
 function formatDate(t, isTime) {
