73395ca9e87c4c211cd853ece0837911c173b728
Administrator <xrzslixiangqun@163.com>
2019-05-26 12:39:42 +0800

    ocsfun

--- asset/js/config.js
+++ asset/js/config.js
@@ -8,4 +8,18 @@
 
 // var $ctx = 'http://192.168.3.10:18080'
 // var $ctx  = 'http://19.14.62.24:18080';
-var $ctx = 'http://172.20.10.4:18080';
+// var $ctx = 'http://172.20.10.4:18080';
+var $ctx = 'http://172.16.5.248:18080';
+
+var $ocsfun = {
+    //公司配置
+    url:'172.16.9.239',
+    dk:9000,
+    username:'admin',
+    password:'admin'
+    //现场配置
+    // url:'19.14.62.1',
+    // dk:9000,
+    // username:'GST0101',
+    // password:'GST0101'
+}
